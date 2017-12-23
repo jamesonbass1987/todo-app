ec0b74f32581011ab5db4bb39a6ca956479a569b
jamesonbass1987 <jamesonbass@gmail.com>
2017-12-22 17:53:28 -0800

    completed basic functionality of todo list

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,13 +1,25 @@
  // Check off Specific Todos By Clicking
 
-$("li").click(function(){
+$("ul").on("click", "li", function(){
  $(this).toggleClass("completed");
 });
 
 // Remove item when 'x' is clicked
-$("span").click(function(event){
+$("ul").on("click", "span", function(event){
   $(this).parent().fadeOut(350, function(){
     $(this).remove();
   })
   event.stopPropagation();
+})
+
+// Add new todo when entered into input
+$("#newTodo").keypress(function(event){
+  if (event.which === 13){
+    //grab text from input
+    let todoText = $(this).val()
+    //create new li and add to ul
+    $('ul').append(`<li class="noselect"><span>X</span> ${todoText}</li>`)
+    //remove value
+    $(this).val("")
+  }
 })
