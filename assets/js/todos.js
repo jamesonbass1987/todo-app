9d26f6c4502c5fd631055421152ffdc6445cbe36
jamesonbass1987 <jamesonbass@gmail.com>
2017-12-22 16:37:50 -0800

    added function to remove list item

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,5 +1,13 @@
  // Check off Specific Todos By Clicking
 
- $("li").click(function(){
-   $(this).toggleClass("completed");
- });
+$("li").click(function(){
+ $(this).toggleClass("completed");
+});
+
+// Remove item when 'x' is clicked
+$("span").click(function(event){
+  $(this).parent().fadeOut(350, function(){
+    $(this).remove();
+  })
+  event.stopPropagation();
+})
